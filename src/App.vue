da8fc9fa4329322ae84ceada800d601b6429661c
florianwoelki <florianwoelki@gmx.de>
2020-07-30 11:20:46 +0200

    feat(view): add search bar and logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <header class="flex shadow p-3 space-x-10">
+    <div class="w-1/3 flex items-center">
+      <img class="h-10 mr-2" src="@/assets/logo.png" alt="logo">
+      <div class="w-full flex items-center bg-gray-200 rounded-full p-2">
+        <svg class="w-6 h-6 text-gray-600" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        <input
+          type="text"
+          placeholder="Search Facebook"
+          class="w-full ml-2 bg-transparent placeholder-gray-600 font-light text-sm"
+        >
+      </div>
+    </div>
+    <div class="w-3/4">
+      Lorem, ipsum.
+    </div>
+    <div class="w-1/3">
+      Lorem, ipsum. 
+    </div>
+  </header>
 </template>
