5aef513f7ba2c46aff6524b74a8c9715f9f5dfa8
florianwoelki <florianwoelki@gmx.de>
2020-07-30 13:43:27 +0200

    feat(view): add badges

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,15 @@
         <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
         <span class="tooltip-text py-2 px-3 absolute z-50 inline-block text-xs rounded-lg bg-gray-700 text-gray-300 font-light">Newsfeed</span>
       </div>
-      <div class="tooltip cursor-pointer my-1 py-2 px-10 border-b-2 border-transparent text-gray-500 hover:bg-gray-200 rounded-lg">
+      <div class="tooltip relative cursor-pointer my-1 py-2 px-10 border-b-2 border-transparent text-gray-500 hover:bg-gray-200 rounded-lg">
         <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20"><path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path></svg>
         <span class="tooltip-text py-2 px-3 absolute z-50 inline-block text-xs rounded-lg bg-gray-700 text-gray-300 font-light">Friends</span>
+        <span class="absolute flex items-center justify-center text-xs top-0 right-0 h-5 w-5 mr-8 mt-1 bg-red-600 text-white rounded-full">7</span>
       </div>
-      <div class="tooltip cursor-pointer my-1 py-2 px-10 border-b-2 border-transparent text-gray-500 hover:bg-gray-200 rounded-lg">
+      <div class="tooltip relative cursor-pointer my-1 py-2 px-10 border-b-2 border-transparent text-gray-500 hover:bg-gray-200 rounded-lg">
         <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd"></path></svg>
         <span class="tooltip-text py-2 px-3 absolute z-50 inline-block text-xs rounded-lg bg-gray-700 text-gray-300 font-light">Watch</span>
+        <span class="absolute flex items-center justify-center text-xs top-0 right-0 h-5 w-5 mr-8 mt-1 bg-red-600 text-white rounded-full">3</span>
       </div>
       <div class="tooltip cursor-pointer my-1 py-2 px-10 border-b-2 border-transparent text-gray-500 hover:bg-gray-200 rounded-lg">
         <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20"><path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z"></path><path fill-rule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -33,12 +35,12 @@
         <span class="tooltip-text py-2 px-3 absolute z-50 inline-block text-xs rounded-lg bg-gray-700 text-gray-300 font-light">Groups</span>
       </div>
     </div>
-    <div class="w-1/3 flex items-center justify-between">
+    <div class="w-2/5 flex items-center justify-between">
       <div class="flex items-center justify-between">
         <img class="h-8 rounded-full mr-1" src="https://randomuser.me/api/portraits/men/76.jpg" alt="profile-image">
         <span class="text-base">Florian</span>
       </div>
-      <div class="space-x-2">
+      <div class="space-x-3">
         <button
           type="button"
           class="bg-gray-300 rounded-full p-2 hover:bg-gray-400"
