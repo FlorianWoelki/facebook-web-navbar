478a1c05d8e1e3c8c45a9731ea02d1ff04342638
florianwoelki <florianwoelki@gmx.de>
2020-08-01 13:10:30 +0200

    feat(plus): add other items for creating button

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,10 @@
             <MenuListItem title="Story" subtitle="Share a photo or write something">
               <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path></svg>
             </MenuListItem>
+            <div class="mx-2 my-2 border-b border-gray-400"></div>
+            <MenuListItem title="Advertisment" subtitle="Easy advertising for your company">
+              <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>
+            </MenuListItem>
           </MenuList>
         </div>
         <button
